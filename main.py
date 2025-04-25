44b20e84a7bbea51f7e7f4b4173f234d88c30bff
Leandro Díaz Calderón <leandroernestodiazcalderon@gmail.com>
2025-04-24 21:51:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ def show_dashboard():
     )
 
     # Agregar un título y subtítulo al gráfico de columnas apiladas
-    fig_col.update_layout(title=go.layout.Title(text="Tendencia Anual de Recolección de <br>Materiales Reciclables (Últimos 5 Años)"))
+    fig_col.update_layout(title=go.layout.Title(text="<br>Tendencia Anual de Recolección de <br>Materiales Reciclables (Últimos 5 Años)"))
 
     # Cambiar los nombres de los materiales de la leyenda de 2 gráficos
     for graph in [fig_line, fig_col]:
